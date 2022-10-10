c91cd868b443643ca4a271e5b53df0155da6288b
Andreas Johansson <87andjoh@gmail.com>
2022-10-10 08:43:47 +0200

    Update init_repo.sh
    Added rename for folder, .csproj and .sln

--- init_repo.sh
+++ init_repo.sh
@@ -4,9 +4,12 @@ read -p 'new namespace: ' ns
 # Replace the namespace in the files
 grep -rli 'Boilerplate.WebApi' * | xargs -i@ sed -i 's/Boilerplate.WebApi/'$ns'/g' @
 
-# rename the boilerplate folders
+# rename the boilerplate folders and proj files
 mv 'Boilerplate.WebApi' $ns
+mv $ns'/Boilerplate.WebApi.csproj' $ns/$ns'.csproj'
 mv 'Boilerplate.WebApi.Tests/' $ns'.Tests'
+mv $ns'.Tests/Boilerplate.WebApi.Tests.csproj' $ns'.Tests'/$ns'.Tests.csproj'
+mv 'Boilerplate.WebApi.sln' $ns.sln
 
 # remove this script
 rm -- "$0"
